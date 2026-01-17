8ba217e5abb248f9f33b629ddae6ff14bf551bb9
pitech01 <pitechofficials@gmail.com>
2026-01-17 15:29:36 +0100

    feat: load patient data on user fetch

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+    return $request->user()->load('patient');
 })->middleware('auth:sanctum');
 
 Route::get('/ping', function () {
