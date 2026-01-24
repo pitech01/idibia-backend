864392de9474f6a46a93919bc935443c6abd60a8
pitech01 <pitechofficials@gmail.com>
2026-01-24 12:17:30 +0100

    doctor update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -134,7 +134,7 @@ class AuthController extends Controller
             ]);
         }
 
-        $user->load('patient');
+        $user->load(['patient', 'doctor']);
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user()->load('patient');
+    return $request->user()->load(['patient', 'doctor']);
 })->middleware('auth:sanctum');
 
 Route::get('/ping', function () {
